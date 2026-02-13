5dcc7ab1c2e507f00e851785b7a42417354d313f
Marwan1137 <marwan.hakil79@gmail.com>
2026-02-13 16:55:22 +0200

    Finished building exceptions & failures files

--- lib/core/error/exceptions.dart
+++ lib/core/error/exceptions.dart
@@ -1,1 +1,34 @@
-class Exceptions {}
+/// Base class for all app exceptions (data & infra layer)
+abstract class AppException implements Exception {
+  final String message;
+
+  const AppException(this.message);
+
+  @override
+  String toString() => message;
+}
+
+/// API / backend related errors
+class ServerException extends AppException {
+  const ServerException(super.message);
+}
+
+/// Local storage (Hive, SharedPreferences, etc)
+class CacheException extends AppException {
+  const CacheException(super.message);
+}
+
+/// No internet, timeout, DNS, etc
+class NetworkException extends AppException {
+  const NetworkException(super.message);
+}
+
+/// 401, 403
+class UnauthorizedException extends AppException {
+  const UnauthorizedException(super.message);
+}
+
+/// 422, bad input, form errors
+class ValidationException extends AppException {
+  const ValidationException(super.message);
+}
--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,1 +1,37 @@
-class Failures {}
+import 'package:equatable/equatable.dart';
+
+/// What the rest of the app understands
+abstract class Failure extends Equatable {
+  final String message;
+
+  const Failure(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure([super.message = "Server error, try again later"]);
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure([super.message = "Local data error"]);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure([super.message = "No internet connection"]);
+}
+
+class UnauthorizedFailure extends Failure {
+  const UnauthorizedFailure([
+    super.message = "Session expired, please login again",
+  ]);
+}
+
+class ValidationFailure extends Failure {
+  const ValidationFailure([super.message = "Invalid input"]);
+}
+
+class UnknownFailure extends Failure {
+  const UnknownFailure() : super("Something went wrong");
+}
